29aec263834acc3569e25fabed192243bced4713
Nath1416 <bougienath@gmail.com>
2025-03-08 14:46:01 -0500

    Added

--- helpme-helpyou/src/app/utils/gemini_gateway.ts
+++ helpme-helpyou/src/app/utils/gemini_gateway.ts
@@ -1,5 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
-
+import {AiAnswer} from  '@/app/class/answer'
+import { json } from 'stream/consumers';
 class GenAIUtils {
 
     genAI: any
@@ -16,11 +17,21 @@ class GenAIUtils {
      */
     async generateContent(prompt:string) {
         try {
-        const result = await this.model.generateContent(prompt);
-        return result.response.text();
+            const result = await this.model.generateContent(prompt);
+            return result.response.text();
         } catch (error) {
-        console.error("Error generating content:", error);
-        throw new Error("Failed to generate content. Please try again.");
+            console.error("Error generating content:", error);
+            throw new Error("Failed to generate content. Please try again.");
+        }
+    }
+
+    async parseResponse(prompt:string): AiAnswer{
+        const answer = await this.generateContent(prompt)
+        try {
+            const parsedData = JSON.parse(answer);
+            return new AiAnswer(parsedData);
+        }catch(error){
+            return new AiAnswer([], true);
         }
     }
 }
