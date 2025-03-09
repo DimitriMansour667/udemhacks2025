aace4e0ce3df84cc3c21ef78a7b5e4d1ef44b937
Nath1416 <bougienath@gmail.com>
2025-03-08 21:28:16 -0500

    Added dict

--- helpme-helpyou/src/app/brain/page.tsx
+++ helpme-helpyou/src/app/brain/page.tsx
@@ -33,19 +33,19 @@ export default function Brain() {
     const [isLoading, setIsLoading] = useState(false);
     const [progress, setProgress] = useState(0);
     const [showingModel, setShowingModel] = useState(false);
-    const points: Point3D[] = [
-        { x: 0.5995514895454759, y: -0.5581046984943983, z: -0.6495908313948302 }, // Cerebellum
-        { x: -0.5307685642102951, y: 0.18521498665199987, z: 0.6060391294560343 }, // Cerebrum
-        { x: 0.23097607679126156, y: -0.7122985424067342, z: 0.12780552084877117 }, // Brainstem
-        { x: -0.5882046695307154, y: 0.6145940866691428, z: 0.02 }, // Frontal lobe
-        { x: 0.27951995256963796, y: 0.7540875925218076, z: 0.02 }, // Parietal lobe
-        { x: -0.9354594627863727, y: 0, z: -0.048221844984680406 }, // Temporal lobe
-        { x: 1.0539338261146913, y: 0.09036493727733252, z: 0.24006206499231558 }, // Occipital lobe
-        { x: 0.3103568635444375, y: -0.9436120129849548, z: 0.01327201803336897 }, // Medulla oblongata
-        { x: 0.08484408250910186, y: 0.5155446110247102, z: -0.5469365826356386 }, // Limbic lobe
-        { x: -0.20268099697845515, y: -0.46522303081001093, z: -0.002686627744875103 }, // Amygdala
-
-    ];
+    const points_dict: { [key: string]: Point3D } = {
+        "Cerebellum": { x: 0.5995514895454759, y: -0.5581046984943983, z: -0.6495908313948302 },
+        "Cerebrum": { x: -0.5307685642102951, y: 0.18521498665199987, z: 0.6060391294560343 },
+        "Brainstem": { x: 0.23097607679126156, y: -0.7122985424067342, z: 0.12780552084877117 },
+        "Frontal lobe": { x: -0.5882046695307154, y: 0.6145940866691428, z: 0.02 },
+        "Parietal lobe": { x: 0.27951995256963796, y: 0.7540875925218076, z: 0.02 },
+        "Temporal lobe": { x: -0.9354594627863727, y: 0, z: -0.048221844984680406 },
+        "Occipital lobe": { x: 1.0539338261146913, y: 0.09036493727733252, z: 0.24006206499231558 },
+        "Medulla oblongata": { x: 0.3103568635444375, y: -0.9436120129849548, z: 0.01327201803336897 },
+        "Limbic lobe": { x: 0.08484408250910186, y: 0.5155446110247102, z: -0.5469365826356386 },
+        "Amygdala": { x: -0.20268099697845515, y: -0.46522303081001093, z: -0.002686627744875103 }
+    };
+    
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         if (!input.trim()) return
@@ -91,9 +91,9 @@ export default function Brain() {
                     <ambientLight intensity={1} />
                     <directionalLight position={[5, 5, 5]} intensity={2} />
                     <OrbitControls enableZoom={true} />
-                    <BrainModel points={points} i={partIndex} />
+                    <BrainModel points={points_dict} i={partIndex} />
                     {showSprite && answer && (
-                            <SpriteComponent data={answer.parts[0]} firstPoint={points[partIndex]}/>
+                            <SpriteComponent data={answer.parts[partIndex]} firstPoint={points_dict[answer.parts[partIndex].part]}/>
                     )}
                 </Canvas>
             </div>
