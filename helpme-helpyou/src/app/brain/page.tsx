e0908e7581eb7f0b2acd782954a37a098b7a1259
Dimitri Mansour <dimi.mansour03@gmail.com>
2025-03-08 19:45:19 -0500

    dsasa

--- helpme-helpyou/src/app/brain/page.tsx
+++ helpme-helpyou/src/app/brain/page.tsx
@@ -9,8 +9,10 @@ import { AiAnswer, Answer } from "../class/answer";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 import * as THREE from 'three';
+import { ModalNathan } from "@/components/ourstuff/modalNathan";
 
 export default function Brain() {
+    
     if(!process.env.NEXT_PUBLIC_GEMINI_API_KEY){
         return <div>No api key error</div>
     }
@@ -21,26 +23,22 @@ export default function Brain() {
     const [input, setInput] = useState("")
     const [answer, setAnswer] = useState<AiAnswer | null>(null)
     const controlsRef = useRef(null);
-
-
-  const points = [
-    {x: -0.5307685642102951, y: 0.18521498665199987, z: 0.6060391294560343}, // Cerebrum
-    {x: 0.5995514895454759, y: -0.5581046984943983, z: -0.6495908313948302}, // Cerebellum
-    {x: 0.23097607679126156, y: -0.7122985424067342, z: 0.12780552084877117}, // Brainstem
-    {x: -0.5882046695307154, y: 0.6145940866691428, z: 0.02}, // Frontal lobe
-    {x: 0.27951995256963796, y: 0.7540875925218076, z: 0.02}, // Parietal lobe
-    {x: -0.9354594627863727, y: 0, z: -0.048221844984680406}, // Temporal lobe
-    {x: 1.0539338261146913, y: 0.09036493727733252, z: 0.24006206499231558}, // Occipital lobe
-    {x: 0.3103568635444375, y: -0.9436120129849548, z: 0.01327201803336897}, // Medulla oblongata
-    {x: 0.08484408250910186, y: 0.5155446110247102, z: -0.5469365826356386}, // Limbic lobe
-    {x: -0.20268099697845515, y: -0.46522303081001093, z: -0.002686627744875103}, // Amygdala
-
-  ];
+    const [modalIsOpen, setModalIsOpen] = useState(false);
+    const [modalTitle, setModalTitle] = useState("");
+    const [modalDescription, setModalDescription] = useState("");
+    const points = [
+        {x: -0.5307685642102951, y: 0.18521498665199987, z: 0.6060391294560343}, // Cerebrum
+        {x: 0.5995514895454759, y: -0.5581046984943983, z: -0.6495908313948302}, // Cerebellum
+        {x: 0.23097607679126156, y: -0.7122985424067342, z: 0.12780552084877117}, // Brainstem
+        {x: -0.5882046695307154, y: 0.6145940866691428, z: 0.02}, // Frontal lobe
+        {x: 0.27951995256963796, y: 0.7540875925218076, z: 0.02}, // Parietal lobe
+        {x: -0.9354594627863727, y: 0, z: -0.048221844984680406}, // Temporal lobe
+        {x: 1.0539338261146913, y: 0.09036493727733252, z: 0.24006206499231558}, // Occipital lobe
+        {x: 0.3103568635444375, y: -0.9436120129849548, z: 0.01327201803336897}, // Medulla oblongata
+        {x: 0.08484408250910186, y: 0.5155446110247102, z: -0.5469365826356386}, // Limbic lobe
+        {x: -0.20268099697845515, y: -0.46522303081001093, z: -0.002686627744875103}, // Amygdala
     
-    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setInput(e.target.value)
-    }
-
+      ];
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault()
         if (!input.trim()) return
@@ -48,9 +46,16 @@ export default function Brain() {
         console.log("Sending message:", input)
         const answer_response = await genAi.parseResponse(input)
         setAnswer(answer_response)
+        if(answer_response.error){
+            setModalTitle("Error");
+            setModalDescription("Try a more relevant question.");
+            setModalIsOpen(true);
+        }
         console.log(answer_response)
         setInput("")
-
+    }
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setInput(e.target.value)
     }
 
     function SpriteComponent({data}) {
@@ -191,6 +196,13 @@ export default function Brain() {
                 <VectorComponent firstPoint={points[partIndex]} secondPoint={[2,0,0]} />
               </Canvas>
             </div>
+            <ModalNathan
+                title={modalTitle}
+                description={modalDescription}
+                isOpen={modalIsOpen}
+                onClose={() => setModalIsOpen(false)}
+            />
+            
             <div className="absolute bottom-[10%] left-1/2 -translate-x-1/2 w-full max-w-2xl px-4">
                 <form onSubmit={handleSubmit} className="flex w-full space-x-2 bg-white/80 backdrop-blur-sm p-6 rounded-lg shadow-lg">
                     <Input
